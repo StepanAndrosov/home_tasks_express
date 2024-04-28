f36c7ce32c0554c21216b4fbcf2c36d11b0e09d6
StepanAndrosov <stepan.news@yandex.ru>
2024-04-28 11:18:12 +0300

    added delete route

--- src/routes/videos.ts
+++ src/routes/videos.ts
@@ -196,46 +196,22 @@ getVideosRouter.put('/:id', (req: RequestWithParamsAndBody<VideoIdParamsModel, V
     res.sendStatus(HTTP_STATUSES.NO_CONTEND_204)
 })
 
+getVideosRouter.delete('/:id', (req: RequestWithParams<VideoIdParamsModel>, res: Response<VideoViewModel>, next: NextFunction) => {
+    const foundVideo = db.videos.find(v => v.id === Number(req.params.id))
+    if (!foundVideo) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+    const foundindex = db.videos.indexOf(foundVideo!)
 
-// app.get('/courses/:id', (req: RequestWithParams<CourseIdParamsModel>, res: Response<CourseViewModel>) => {
-//     const foundCourse = db.courses.find(c => c.id === + req.params.id)
+    if (foundindex < 0) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+    db.videos.splice(foundindex, 1)
 
-//     if (!foundCourse) {
-//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-//         return
-//     }
-//     res.json({ id: foundCourse.id, title: foundCourse.title })
-//     res.sendStatus(HTTP_STATUSES.OK_200)
-// })
-
-// app.post('/courses', (req: RequestWithBody<CourseCreateModel>, res: Response<CourseViewModel>) => {
-
-//     if (!req.body.title) {
-//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-//         return
-//     }
-//     const createdCourse = {
-//         id: db.courses.length + 1,
-//         title: req.body.title,
-//         studentsCount: 0
-//     }
-//     db.courses.push(createdCourse)
-//     res.sendStatus(HTTP_STATUSES.CREATED_201)
-//     res.json(createdCourse)
-// })
-
-// app.delete('/courses/:id', (req: RequestWithParams<CourseIdParamsModel>, res: Response) => {
-
-//     const findId = db.products.filter((c) => c.id === +req.params.id)[0].id
-//     console.log(findId, 'findId')
-//     if (findId > -1) {
-//         db.courses.splice(findId - 1, 1)
-//         res.sendStatus(HTTP_STATUSES.NO_CONTEND_204)
-//     } else {
-//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-//         return
-//     }
-// })
+    res.sendStatus(HTTP_STATUSES.NO_CONTEND_204)
+})
 
 
 
