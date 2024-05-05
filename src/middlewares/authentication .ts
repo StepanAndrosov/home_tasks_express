59a5caac3f4bde1b9eae1f70ec71d9892de33847
StepanAndrosov <stepan.news@yandex.ru>
2024-05-05 14:33:50 +0300

    check title

--- src/middlewares/authentication .ts
+++ src/middlewares/authentication .ts
@@ -2,16 +2,17 @@
 import { NextFunction, Request, Response } from "express"
 import { HTTP_STATUSES } from "../utils"
 // admin\qwerty.
+const authTitle = 'Basic'
 const auth = { login: 'admin', password: 'qwerty' }
 
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     // parse login and password from headers
+    const title = (req.headers.authorization || '').split(' ')[0] || ''
     const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-
     // Verify login and password are set and correct
-    if (login && password && login === auth.login && password === auth.password) {
+    if (login && password && login === auth.login && password === auth.password && title === authTitle) {
         // Access granted...
         return next()
     }
