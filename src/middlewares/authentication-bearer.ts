1b28d953ca72a951b57b0e13e0175c5f85899851
StepanAndrosov <stepan.news@yandex.ru>
2024-06-23 09:36:43 +0300

    fix

--- src/middlewares/authentication-bearer.ts
+++ src/middlewares/authentication-bearer.ts
@@ -14,7 +14,7 @@ export const authenticationBearerMiddleware = (req: Request, res: Response<JWTPa
     // Verify token is set and correct
     console.log(decoded)
 
-    if (decoded?.exp! < Math.floor(Date.now() / 1000)) {
+    if (decoded?.exp! * 1000 < Math.floor(Date.now())) {
         console.log(HTTP_STATUSES.FORBIDDEN_403)
         res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
         return
